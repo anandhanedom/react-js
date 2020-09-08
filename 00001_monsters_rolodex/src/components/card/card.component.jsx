bf672f86964faa33e5a369e59fe1d17554c04b37
Anandhan <anandhanedom@gmail.com>
2020-09-09 01:26:56 +0530

    progressing

--- 00001_monsters_rolodex/src/components/card/card.component.jsx
+++ 00001_monsters_rolodex/src/components/card/card.component.jsx
@@ -3,8 +3,13 @@ import './card.styles.css';
 
 export const Card = (props) => {
   return (
-    <div className="">
-      <h1>{props.monster.name}</h1>
+    <div className="card-container">
+      <img
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+        alt=""
+      />
+      <h2>{props.monster.name}</h2>
+      <p>{props.monster.email}</p>
     </div>
   );
 };
