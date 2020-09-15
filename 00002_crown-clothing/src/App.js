3efc12a331dbfe245a7e49d305fd541fc05de841
Anandhan <anandhanedom@gmail.com>
2020-09-15 13:33:25 +0530

    notes updates

--- 00002_crown-clothing/src/App.js
+++ 00002_crown-clothing/src/App.js
@@ -5,12 +5,20 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
       </Switch>
     </div>
   );
