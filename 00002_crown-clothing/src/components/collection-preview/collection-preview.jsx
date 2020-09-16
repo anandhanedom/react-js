1eced097c3b3dfbbc29430aaaa7b0946c4d1cd42
Anandhan <anandhanedom@gmail.com>
2020-09-16 12:34:42 +0530

    added images shop

--- 00002_crown-clothing/src/components/collection-preview/collection-preview.jsx
+++ 00002_crown-clothing/src/components/collection-preview/collection-preview.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-import './collection-preview';
+import CollectionItem from '../collection-item/collection-item.component';
+
+import './collection-preview.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
-        .filter((item,i) => i< 4)
-        .map((item) => (
-          <div key={item.id}>{item.name}</div>
+        .filter((item, i) => i < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
   </div>
