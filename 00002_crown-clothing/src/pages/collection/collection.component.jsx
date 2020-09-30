820578a57a4a8e8f11a21aeaf3a63baad362c4cb
Anandhan <anandhanedom@gmail.com>
2020-09-30 11:22:58 +0530

    till section 11

---
+++ 00002_crown-clothing/src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import collectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => (
+  <div className="category">
+    <h2>Collection Page</h2>
+  </div>
+);
+
+export default CollectionPage;
---
+++ 00002_crown-clothing/src/redux/shop/shop.selectors.js
@@ -0,0 +1,10 @@
+import { createSelector } from 'reselect';
+
+const COLLECTION_ID_MAP = {};
+
+const selectShop = (state) => state.shop;
+
+export const selectCollections = createSelector(
+  [selectShop],
+  (shop) => shop.collections
+);
