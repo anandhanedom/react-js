477e9ef3f9c37c74972c6044e85c74ef0718fdf5
Anandhan <anandhanedom@gmail.com>
2020-10-09 20:33:16 +0530

    material ui tutorial

--- 000003_materialui-react/src/App.js
+++ 000003_materialui-react/src/App.js
@@ -2,22 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonGroup variant="contained">
+          {' '}
+          <Button startIcon={<SaveIcon />} size="large" color="primary">
+            Save
+          </Button>
+          <Button startIcon={<DeleteIcon />} size="large" color="secondary">
+            Discard
+          </Button>
+        </ButtonGroup>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
